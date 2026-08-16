ae59e7e84932453bf576c6d0c967d15a1b436bbe
Heikuroh <black.crow.claws@gmail.com>
2026-08-16 15:19:15 +0900

    ルーティングの変更

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: "articles#index"
-  resources :articles
+  resources :articles do
+    resources :comments, only: [ :new, :create ]
+  end
 
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
