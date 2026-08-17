d28b8c80af9a7a53cd71a58fcce93ce0710e2460
Heikuroh <black.crow.claws@gmail.com>
2026-08-17 22:10:20 +0900

    年齢の表示

--- app/models/user.rb
+++ app/models/user.rb
@@ -25,12 +25,18 @@ class User < ApplicationRecord
   has_many :articles, dependent: :destroy
   has_one :profile, dependent: :destroy
 
+  delegate :birthday, :age, :gender, to: :profile, allow_nil: true
+
   def has_written?(article)
     articles.exists?(id: article.id)
   end
 
   def display_name
-    self.email.split("@").first
+    profile&.nickname || self.email.split("@").first
+  end
+
+  def prepare_profile
+    profile || build_profile
   end
 
   def prepare_profile
